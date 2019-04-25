2f404be078069e7ef1aef0a91d2203a41e85f388
David Melin <davidmmelin@gmail.com>
2019-04-25 16:05:55 -0700

    ERB booleans working

--- app/javascript/hello_angular/app/app.component.erb.ts
+++ app/javascript/hello_angular/app/app.component.erb.ts
@@ -9,6 +9,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public name: string = 'Angular!';
   public str: string = '<%= "#{Rails.env} on local" %>';
+  public bool: boolean = JSON.parse('<%= false %>');
   public arr: string[] = JSON.parse('<%= ["Music","Video","Book"] %>');
   public obj: any = JSON.parse('<%= {"key"=> "chain", "pointer" => "laser"}.to_json %>');
   public objArr: any = JSON.parse('<%= {arr: ["dog","cat"], obj: {sound: "woof"}, objArr: [{name: "David"}, {name: "Michael"}]}.to_json %>');
